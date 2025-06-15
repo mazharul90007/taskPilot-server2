b966e352f33f25ad76eb094f7382c88cf2a2c2a4
smhasanjamil <smhasanjamil23@gmail.com>
2025-06-15 22:52:48 +0600

    solved merge conflict route/index

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,29 +1,10 @@
-<<<<<<< HEAD
-import express from 'express';
-import userRoutes from '../modules/user/user.routes';
-
-// import { AdminRoutes } from '../modules/Admin/admin.routes';
-// import { AuthRoutes } from '../modules/Auth/auth.routes';
-=======
 import express from "express";
 import { userRoutes } from "../modules/user/user.route";
 import { authRoutes } from "../modules/auth/auth.route";
->>>>>>> e67216b38336348834fad1f9723562caf6dd63d6
 
 const router = express.Router();
 
 const moduleRoutes = [
-<<<<<<< HEAD
-    {
-        path: '/users',
-        route: userRoutes
-    }
-];
-
-moduleRoutes.forEach(route => router.use(route.path, route.route));
-
-export default router;
-=======
   {
     path: "/user",
     route: userRoutes,
@@ -37,4 +18,3 @@ export default router;
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
 
 export default router;
->>>>>>> e67216b38336348834fad1f9723562caf6dd63d6
