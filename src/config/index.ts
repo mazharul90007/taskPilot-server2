e67216b38336348834fad1f9723562caf6dd63d6
smhasanjamil <smhasanjamil23@gmail.com>
2025-06-15 21:11:46 +0600

    Authentication added

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,4 +7,8 @@ export default {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
   NODE_ENV: process.env.NODE_ENV,
+  jwt_access_secret: process.env.JWT_ACCESS_SECRET,
+  jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
+  jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
+  jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
 };
