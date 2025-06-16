19b39b45cfdd4d81be1ec5f81745fa1644943b4e
smhasanjamil <smhasanjamil23@gmail.com>
2025-06-17 05:00:13 +0600

    Updated

--- dist/config/index.js
+++ dist/config/index.js
@@ -14,4 +14,6 @@ exports.default = {
     jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
     jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
     jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
+    stripe_secret_key: process.env.STRIPE_SECRET_KEY,
+    frontend_base_url: process.env.FRONTEND_BASE_URL,
 };
